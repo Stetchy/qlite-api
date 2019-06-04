4ba297666f277ccb470d3d43410f01d5d5ad6444
Riain Condon <riain.condon4@mail.dcu.ie>
2019-06-04 22:30:49 +0100

    Update CORS

--- src/main/java/condonr4/api/config/WebSecurityConfig.java
+++ src/main/java/condonr4/api/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.cors().and().csrf().disable().authorizeRequests()
                 .anyRequest().permitAll();
     }
 
